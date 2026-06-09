3a80bd7495d5bf18640daf996a58682bbf1a219f
hal <halt0413@gmail.com>
2026-06-09 11:42:01 +0900

    グラデーションスタイルの整形を修正

--- src/components/layout/gradientLayout.ts
+++ src/components/layout/gradientLayout.ts
@@ -1,7 +1,6 @@
 import type { CSSProperties } from "react";
 
 export const gradients = {
-  green:
-    "linear-gradient(180deg, #50DC7C 10%, #5BECB7 99%) padding-box, linear-gradient(0deg, #50DC7C 10%, #5BECB7 99%) border-box",
+  green: "linear-gradient(180deg, #50DC7C 10%, #5BECB7 99%) padding-box, linear-gradient(0deg, #50DC7C 10%, #5BECB7 99%) border-box",
   blue: "linear-gradient(180deg, #0080F7 47%, #68B6FF 99%) padding-box, linear-gradient(0deg, #0080F7 10%, #68B6FF 99%) border-box",
 } satisfies Record<string, CSSProperties["background"]>;
