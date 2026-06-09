c022d43e14f0997ea50c7be0cce2590a93072197
okonakona <2240057@ecc.ac.jp>
2026-06-09 11:24:07 +0900

    fix: 画像のファイル構成の見直し

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,14 @@
+export default function Page() {
+  return (
+    <main className="min-h-svh bg-[url('/images/home/background.png')] bg-cover bg-center">
+      <div className="flex min-h-svh items-center justify-center">
+        <h1 className="text-5xl font-bold text-white">Home</h1>
+      </div>
+      <img
+        src="/images/home/effect.png"
+        alt=""
+        className="fixed max-w-fit left-0 bottom-0"
+      />
+    </main>
+  );
+}
