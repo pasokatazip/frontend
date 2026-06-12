786fdd35aa86225140ca36c6feae5b72b77bc7d1
hal <halt0413@gmail.com>
2026-06-12 12:27:03 +0900

    ToggleButtonコンポーネントを追加し、スタイルと機能を定義

--- src/components/layout/shadowLayout.ts
+++ src/components/layout/shadowLayout.ts
@@ -4,4 +4,5 @@ export const shadows = {
   green: "0 0 5px #009C57, inset 0 4px 10px #B8FFDF",
   blue: "0 0 5px #0041FF, inset 0 4px 10px #98D9FF",
   silver: "0 0 5px #5BD4EC, inset 0 4px 10px #FFFFFF",
+  black: "0 0 4px #525252",
 } satisfies Record<string, CSSProperties["boxShadow"]>;
