0e90aa37ed9dcdfbba3f4d81af7aea78252ad2df
okonakona <2240057@ecc.ac.jp>
2026-06-23 09:41:10 +0900

    fix: ハンバーガーメニューの追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,46 @@
+"use client";
+
 import { GreenButton } from "@/components/ui/button/GreenButton";
 import { RoundButton } from "@/components/ui/button/RoundButton";
+import { Hamburger } from "@/components/Hamburger";
+import { useState } from "react";
+import clsx from "clsx";
 
 export function Footer() {
+  const [openMenu, setMenu] = useState(false);
+  const menuFunction = () => {
+    setMenu(!openMenu);
+  };
   return (
     <footer className="fixed bottom-10 left-0 min-w-full px-4">
       <nav className="flex justify-center gap-5">
         <RoundButton image="/icons/book.svg" label="きろく" className="" />
-        <GreenButton
-          children="つぶやく"
-          className="rounded-xl rounded-bl-xs max-h-12.5 min-w-40"
+        <GreenButton className="max-h-12.5 min-w-40 rounded-xl rounded-bl-xs">
+          つぶやく
+        </GreenButton>
+        <RoundButton
+          image="/icons/menu.svg"
+          label="メニュー"
+          className=""
+          onClick={menuFunction}
         />
-        <RoundButton image="/icons/menu.svg" label="メニュー" className="" />
       </nav>
+      <div
+        onClick={() => setMenu(false)}
+        className={clsx(
+          "fixed inset-0 bg-black transition-opacity duration-300 pointer-events-none",
+          openMenu ? "opacity-25" : "opacity-0 pointer-events-none",
+        )}
+      />{" "}
+      <Hamburger
+        onClose={() => setMenu(false)}
+        className={clsx(
+          "transition-all duration-300",
+          openMenu
+            ? "translate-x-0 opacity-100"
+            : "translate-x-full opacity-0 pointer-events-none",
+        )}
+      />
     </footer>
   );
 }
---
+++ src/features/home/components/HomeView.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import { Footer } from "@/components/Footer";
+
+type HomeViewProps = {
+  effectImage: {
+    height: number;
+    src: string;
+    width: number;
+  };
+  title: string;
+};
+
+export function HomeView({ effectImage, title }: HomeViewProps) {
+  return (
+    <>
+      <main className="min-h-svh bg-[url('/images/home/background.png')] bg-cover bg-center">
+        <Image
+          src={effectImage.src}
+          alt=""
+          width={effectImage.width}
+          height={effectImage.height}
+          className="fixed bottom-0 left-0 max-w-fit"
+        />
+      </main>
+      <Footer />
+    </>
+  );
+}
