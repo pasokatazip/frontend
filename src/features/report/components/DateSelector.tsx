904e7d68b4bf24443c66433c2da338d0e21430b2
okonakona <2240057@ecc.ac.jp>
2026-06-30 23:03:02 +0900

    feature: 左右で日付指定、真ん中文字でカレンダー出るように

---
+++ src/features/report/components/DateSelector.tsx
@@ -0,0 +1,39 @@
+import { SilverButton } from "@/components/ui/button/SilverButton";
+
+type DateSelectorProps = {
+  text: string;
+  onPrev: () => void;
+  onNext: () => void;
+  onOpen: () => void;
+};
+
+export function DateSelector({
+  text,
+  onPrev,
+  onNext,
+  onOpen,
+}: DateSelectorProps) {
+  return (
+    <SilverButton className="max-h-10 max-w-full p-2 flex justify-evenly gap-2.5 text-[16px]">
+      <button onClick={onPrev}>
+        <span
+          className="
+            block w-6 h-6
+            bg-[url('/icons/arrowFront.svg')]
+            bg-contain bg-no-repeat            "
+        />
+      </button>
+
+      <button onClick={onOpen}>{text}</button>
+
+      <button onClick={onNext}>
+        <span
+          className="
+            block w-6 h-6
+            bg-[url('/icons/arrowBack.svg')]
+            bg-contain bg-no-repeat            "
+        />
+      </button>
+    </SilverButton>
+  );
+}
