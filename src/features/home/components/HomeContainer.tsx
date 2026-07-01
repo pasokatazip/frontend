1ea334b68ecee0696b98d4f8e63a6955928cc29a
okonakona <2240057@ecc.ac.jp>
2026-07-01 12:04:30 +0900

    fix: title消しきれてなかった

--- src/features/home/components/HomeContainer.tsx
+++ src/features/home/components/HomeContainer.tsx
@@ -3,7 +3,6 @@ import { HomeView } from "./HomeView";
 export function HomeContainer() {
   return (
     <HomeView
-      title="Home"
       effectImage={{
         src: "/images/home/effect.png",
         width: 1125,
