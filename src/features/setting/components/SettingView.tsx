1e0369854ea6b1e602bb40c1aa371e7d9d634d98
okonakona <2240057@ecc.ac.jp>
2026-07-15 18:49:10 +0900

    fix: 設定画面差し替え

--- src/features/setting/components/SettingView.tsx
+++ src/features/setting/components/SettingView.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import { TextButton } from "@/components/ui/button/TextButton";
 import { NotificationSettingSection } from "@/features/setting/components/NotificationSettingSection";
-import { PetSettingSection } from "@/features/setting/components/PetSettingSection";
+import { PetLockedOverlay } from "@/features/setting/components/PetLockedOverlay";
 import { SettingFooter } from "@/features/setting/components/SettingFooter";
+import { PetSettingSection } from "@/components/ui/color/PetSettingSection";
+import Link from "next/link";
+import { BlueButton } from "@/components/ui/button/BlueButton";
 
 type SettingViewProps = {
   petImage: {
@@ -14,6 +20,7 @@ type SettingViewProps = {
 };
 
 export function SettingView({ petImage }: SettingViewProps) {
+  const [hue, setHue] = useState(0);
   return (
     <main className="mobile-safe-scroll-screen relative overflow-hidden bg-[url('/images/background.png')] bg-cover bg-[position:center_top] bg-no-repeat px-4">
       <div className="mobile-safe-content mx-auto flex w-full max-w-[28rem] flex-col">
@@ -30,13 +37,22 @@ export function SettingView({ petImage }: SettingViewProps) {
           </h1>
         </header>
 
-        <PetSettingSection petImage={petImage} />
-
+        <section className="mt-8">
+          <h2 className="text-base leading-6 font-normal tracking-normal text-[#4C4F5E]">
+            ペット設定
+          </h2>
+          <PetSettingSection hue={hue} onHueChange={setHue}></PetSettingSection>
+        </section>
         <NotificationSettingSection />
 
         <TextButton className="mt-14 w-fit text-base leading-6 font-normal tracking-normal text-red-600 underline underline-offset-2">
           アカウント削除
         </TextButton>
+        <Link href="/Home" className="w-full flex my-6">
+          <BlueButton className="max-h-15 max-w-[85%] m-auto text-[18px]">
+            設定を保存する
+          </BlueButton>
+        </Link>
 
         <SettingFooter petImage={petImage} />
       </div>
