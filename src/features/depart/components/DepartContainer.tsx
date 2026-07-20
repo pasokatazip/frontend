c91c7218719c4d45f82ce1db1adcddcccf129c74
sojiro takagi <soujirou.takagi@gmail.com>
2026-07-20 21:45:38 +0900

    #37 add:旅立ちページ一枚目作成

--- src/features/depart/components/DepartContainer.tsx
+++ src/features/depart/components/DepartContainer.tsx
@@ -1,5 +1,14 @@
 import { DepartView } from "./DepartView";
 
 export function DepartContainer() {
-    return <DepartView />;
+    return (
+        <DepartView
+            name="YoYo"
+            effectImage={{
+                src: "/images/home/effect.png",
+                width: 1125,
+                height: 1143,
+            }}
+        />
+    );
 }
--- src/features/depart/components/DepartView.tsx
+++ src/features/depart/components/DepartView.tsx
@@ -1,7 +1,32 @@
-export function DepartView() {
+import Image from "next/image";
+import { TopMessagePanel } from "@/components/ui/panel/TopMessagePanel";
+
+type DepartViewProps = {
+    effectImage: {
+        height: number;
+        src: string;
+        width: number;
+    };
+    name: string;
+};
+
+export function DepartView({ effectImage, name }: DepartViewProps) {
     return (
-        <div>
-            <p>テスト</p>
-        </div>
+        <>
+            <main className="mobile-screen bg-[url('/images/home/background.png')] bg-cover bg-center">
+                <Image
+                    src={effectImage.src}
+                    alt=""
+                    width={effectImage.width}
+                    height={effectImage.height}
+                    className="mobile-safe-bottom-0 fixed left-0 max-w-fit"
+                />
+                <TopMessagePanel>
+                    {name}は、あなたに
+                    <br />
+                    伝えたいことがあるようです
+                </TopMessagePanel>
+            </main>
+        </>
     );
 }
