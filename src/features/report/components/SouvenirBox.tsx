552745747ee3ed68487f2c24f7fd54ad3141f4f3
takajirou <soujirou.takagi@gmail.com>
2026-07-27 14:53:18 +0900

    #133 fix:SouveirBoxを左揃えに修正

--- src/features/report/components/SouvenirBox.tsx
+++ src/features/report/components/SouvenirBox.tsx
@@ -1,5 +1,6 @@
 import { GlassCard } from "@/components/ui/card/GlassCard";
 import Image from "next/image";
+import clsx from "clsx";
 import type { Souvenir } from "@/types/souvenir";
 
 type Props = {
@@ -10,13 +11,18 @@ export function SouvenirBox({ souvenirs }: Props) {
   return (
     <GlassCard className="w-full rounded-xl bg-white/30 p-5 text-[10px] text-[#4C4F5E]">
       <p>おみやげ</p>
-      <div className="flex justify-between mt-2">
-        {souvenirs.map((souvenir) => (
+      <div className="mt-2 grid grid-cols-3">
+        {souvenirs.map((souvenir, index) => (
           <Image
             key={souvenir.id}
             src={souvenir.image}
             alt=""
-            className="object-cover"
+            className={clsx(
+              "object-cover",
+              index === 0 && "justify-self-start",
+              index === 1 && "justify-self-center",
+              index === 2 && "justify-self-end",
+            )}
             width={50}
             height={50}
             unoptimized
