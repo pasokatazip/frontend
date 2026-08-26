bc0dfbbfe0e5308d760c46b14458842ecdddb25f
jirou <soujirou.takagi@gmail.com>
2026-08-26 17:39:19 +0900

    #196  feat:404ページ雛形作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFoundContainer } from "@/features/not-found/components/NotFoundContainer";
+
+export default function NotFoundPage() {
+  return <NotFoundContainer />;
+}
---
+++ src/features/not-found/components/NotFounfView.tsx
@@ -0,0 +1,5 @@
+export function NotFoundView() {
+  return (
+    <main className="login-screen mobile-screen auth-screen flex justify-center bg-[url('/images/top/background.png')] bg-cover bg-center px-6"></main>
+  );
+}
